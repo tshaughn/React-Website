d96b3d497e59f29e138f2f6c89f2a8fc62e483c1
Shaughnessy <shaughnt@email.sc.edu>
2024-06-16 22:41:47 -0400

    finished navbar and header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-import { Footer, Blog, Possibiliity, Features, 
+import { Footer, Blog, Possibility, Features, 
         WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
